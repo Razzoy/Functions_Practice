05d1e35ae2c73c3940091b83d6aa018e31e66e41
Razzoy <1081892@ucn.dk>
2023-11-15 09:46:28 +0100

    2nd itteration
    'nuff said

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,6 +7,27 @@ MyCoolFunction('warning 1');
 MyCoolFunction('warning 2');
 MyCoolFunction('ikke en warning');
 
+
+// PLUS +++++
+console.group('MyAddition');
+
+let myAdditionSum = MyAddition(176, 287);
+console.log(myAdditionSum);
+
+console.groupEnd();
+
+// GANGE *****
+console.group('MyProduct');
+
+let myProductSum = MyProduct(7, 3);
+console.log(myProductSum);
+
+console.groupEnd();
+
+
+
+// FUNCTIONS -----------------------------------------------------------
+
 function MyCoolFunction(myWarningText) {
 
     console.log(myWarningText);
@@ -18,24 +39,17 @@ function MyOtherFunction() {
     console.log('hej fra MyOtherFunction')
 }
 
-// PLUS +++++
-let myAdditionSum = MyAddition(176, 287);
-
-console.log(myAdditionSum);
-
+// plusser to værdier, retunerer summen af 2 'numbers'
 function MyAddition(valueOne, valueTwo){
 
-    return valueOne + valueTwo;
+    let result = valueOne + valueTwo;
+    return result;
 }
 
-// GANGE *****
-let myMultiplySum = MyMultiply(7, 3);
+// Ganger to værdier, retunerer summen af 2 'numbers'
+function MyProduct(valueOne, valueTwo){
 
-console.log(myMultiplySum);
-
-function MyMultiply(valueOne, valueTwo){
-
-    return valueOne * valueTwo;
+    let result = valueOne * valueTwo;
+    return result;
 }
-
 
